d276f169da9f6bd8e23b4292a859ddf25accadce
TKratter <tomkratter@gmail.com>
2022-07-14 10:44:34 +0300

    added gt creation notebook

--- src/training/data.py
+++ src/training/data.py
@@ -32,7 +32,7 @@ from open_clip import tokenize
 class CsvDataset(Dataset):
     def __init__(self, input_filename, transforms, img_key, caption_key, sep="\t"):
         logging.debug(f'Loading csv data from {input_filename}.')
-        df = pd.read_csv(input_filename, sep=sep)
+        df = pd.read_csv(input_filename)
 
         self.images = df[img_key].tolist()
         self.captions = df[caption_key].tolist()
